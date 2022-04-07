c1349291aad8f4ba9d70a10dd11b3a8ed8d11f8f
Karen Mayumi Hirayama <karenm.hirayama@gmail.com>
2022-04-06 21:50:34 -0300

    Get input value test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { Input } from './components/Input/Input';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/components/Input/Input.test.tsx
+++ src/components/Input/Input.test.tsx
@@ -1,10 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Input } from './Input';
 
 describe("Input Component", () => {
-    test("Rendered Input", () => {
+    it("Rendered Input", () => {
         render(<Input />);
         const input = screen.getByTestId('inputTodo');
         expect(input).toBeTruthy();
+    });
+
+    it("Get input change", () => {
+        render(<Input/>);
+        const input = screen.getByTestId('inputTodo');
+        const wordInputTest = 'Word Input';
+        fireEvent.change(input, {target: {value: wordInputTest}})
     })
   })
